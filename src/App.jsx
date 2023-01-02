53b1fc755220fe42aaa9a3faaa77de4047b0fe80
JoseLuisHcoder <zonajl@gmail.com>
2023-01-02 16:59:28 -0400

    crud complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,68 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import axios from 'axios'
+import CardUsers from './components/CardUsers'
+import FormUser from './components/FormUser'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [users, setUsers] = useState()
+  const [updateInfo, setUpdateInfo] = useState()
+
+  // console.log(updateInfo);
+
+  const getAllUsers = () => {
+    const URL = 'https://users-crud.academlo.tech/users/'
+    axios.get(URL)
+      .then(res => setUsers(res.data))
+      .catch(err => console.log(err))
+  }
+  useEffect(() => {
+    getAllUsers()
+  }, [])
+
+  const createNewUser = data => {
+    const URL = 'https://users-crud.academlo.tech/users/'
+    axios.post(URL, data)
+      .then(() => getAllUsers())
+      .catch(err => console.log(err))
+  }
+
+const deleteUserById = id => {
+  const URL = `https://users-crud.academlo.tech/users/${id}/`
+  axios.delete(URL)
+    .then(() => getAllUsers())
+    .catch(err => console.log(err))
+}
+
+const updateUserById = (id, data) => {
+  const URL = `https://users-crud.academlo.tech/users/${id}/`
+  axios.put(URL, data)
+    .then(res => getAllUsers())
+    .catch(err => console.log(err))
+
+}
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>CRUD USERS</h1>
+      <FormUser 
+        createNewUser={createNewUser}
+        updateInfo={updateInfo}
+        updateUserById={updateUserById}
+        setUpdateInfo={setUpdateInfo}
+      />
+      {
+        users?.map(user => (
+          <CardUsers 
+          key={user.id}
+          user={user}
+          deleteUserById={deleteUserById}
+          setUpdateInfo={setUpdateInfo}
+          />
+        ))
+      }
+      
     </div>
   )
 }
